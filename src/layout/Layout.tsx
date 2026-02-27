7a1bf5fb7694b9fb2a9d1d32e76c859b1f40ba30
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-27 21:14:08 +0900

    fix: footer 폴더 구조 수정

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-import { Footer } from "react-day-picker";
+import Footer from "../components/Footer";
 
 export default function Layout() {
   return (
