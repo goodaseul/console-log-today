dcb2a4c8035bef3296445303ea8f7a8d9125d237
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-05 15:29:26 +0900

    feature: dark 모드

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "../Button";
 import { signOut } from "@/api/auth.api";
 import { useAuthStore } from "@/stores/auth.store";
+import { useThemeStore } from "@/stores/theme.store";
 
 export default function Header() {
   const user = useAuthStore((state) => state.user);
   const clearUser = useAuthStore((state) => state.clearUser);
 
-  const [dark, setDark] = useState(true);
+  const theme = useThemeStore((state) => state.theme);
+  const toggleTheme = useThemeStore((state) => state.toggleTheme);
   const handleLogout = async () => {
     await signOut();
     clearUser();
@@ -21,12 +22,8 @@ export default function Header() {
         </Link>
       </h1>
       <div className="flex items-center gap-2">
-        <Button
-          onClick={() => setDark((prev) => !prev)}
-          variant="light"
-          className="shadow-2xl"
-        >
-          {dark ? "☀️" : "🌙"}
+        <Button onClick={toggleTheme} variant="light" className="shadow-2xl">
+          {theme === "dark" ? "☀️" : "🌙"}
         </Button>
         {!user ? (
           <Link to={"/login"}>🔒</Link>
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
+import { useThemeStore } from "@/stores/theme.store";
+import { useEffect } from "react";
 
 export default function Layout() {
+  const theme = useThemeStore((state) => state.theme);
+  useEffect(() => {
+    const html = document.documentElement;
+    if (theme === "dark") {
+      html.classList.add("dark");
+    } else {
+      html.classList.remove("dark");
+    }
+  }, [theme]);
+
+  console.log("theme: ", theme);
   return (
-    <>
+    <div className="min-h-screen bg-background text-foreground">
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 }
