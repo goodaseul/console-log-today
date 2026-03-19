d006fff38c34b15d54ca8b8d3beb6ce9aefbd5b2
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-19 15:10:30 +0900

    refactor: 쿼리키 수정

--- src/hooks/queries/queryKey.ts
+++ src/hooks/queries/queryKey.ts
@@ -3,7 +3,7 @@ export const queryKeys = {
     all: ["diary"],
     lists: () => [...queryKeys.diary.all, "list"] as const,
     detail: (userId: string | undefined, date: string) =>
-      [...queryKeys.diary.all, userId, date] as const,
+      [...queryKeys.diary.all, "detail", userId, date] as const,
     month: (userId: string | undefined, yearMonth: string) =>
       [...queryKeys.diary.all, "month", userId, yearMonth] as const,
     count: (userId: string | undefined) =>
